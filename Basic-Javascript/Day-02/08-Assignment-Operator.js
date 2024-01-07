af361493a34e740996712f09ce1ff25e0fde2a55
joyalshaji135 <joyalshaji135@gmail.com>
2024-01-07 22:31:54 +0530

    Assignment Operations

---
+++ Basic-Javascript/Day-02/08-Assignment-Operator.js
@@ -0,0 +1,38 @@
+// Assignment Operation in Javascript
+
+let assignnumber1 = 34;
+
+assign = assignnumber1;
+
+console.log("assign =",assign);
+
+let Num1 = 10;
+
+Num1 += 4; // Num1 = Num1 + 4
+console.log("num1 = ",Num1);
+
+let Num2 = 10;
+
+Num2 -= 4; //Num2 = Num2 - 4;
+console.log("num2 = ",Num2);
+
+let Num3 = 10;
+
+Num3 *= 4; //Num3 = Num3 * 4;
+console.log("num3 = ",Num3);
+
+let Num4 = 10;
+
+Num4 /= 4; //Num4 = Num4 / 4;
+console.log("num4 = ",Num4);
+
+let Num5 = 10;
+
+Num5 %= 4; //Num5 = Num5 % 4;
+console.log("num5 = ",Num5);
+
+let Num6 = 2;
+
+Num6 **= 4; //Num6 = Num6 ** 4; power is 4
+console.log("num6 = ",Num6); 
+
