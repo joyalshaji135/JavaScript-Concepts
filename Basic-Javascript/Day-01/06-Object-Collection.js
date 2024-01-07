36603a7fe2cd1c4f4803a041514870835c20aa0d
joyalshaji135 <joyalshaji135@gmail.com>
2024-01-07 19:43:56 +0530

    Day 1 Is Completed

---
+++ Basic-Javascript/Day-01/06-Object-Collection.js
@@ -0,0 +1,58 @@
+// Object Declaration ==> Collection of Different Variable
+
+// Student  
+
+// Workouts Plan
+
+
+const Student = {
+    FullName : "Joyal Shaji",
+    Age : 23,
+    cgpa : 7.4,
+    IsPass : true
+};
+
+console.log(Student);
+
+Student["Age"] = Student["Age"] + 1;
+// Change the Values
+
+Student["FullName"] = "Sreemayi CM";
+
+// Linkedin Data Fetch
+
+const LinkedinAvatar = {
+    UserName : "Joyal Shaji",
+    JobTitle : "Full Stack Developer",
+    isFollow : true,
+    CurrentPlace : "UP",
+    CurrentStatus : false
+};
+
+LinkedinAvatar["UserName"] = "Sunny Varghese";
+
+LinkedinAvatar[isFollow] = false;
+
+
+console.log(LinkedinAvatar);
+
+const Product = {
+    ProductName : "Praker Jotter Standard CT Ball Pen (Black)",
+    ProductRating : 4.2,
+    ProductPrice : 300,
+    ProductOffer : 5
+};
+
+console.log(Product);
+
+const InstagramAvt = {
+    InstaName : "_jo_y_al_",
+    InstaPostCount : 20,
+    InstaFollowers : 200,
+    InstaFollowing : 400,
+    isFollowStatus : true,
+    InstaUserName : "Joyal Shaji",
+    InstaBio : "Something Wrong"
+};
+
+console.log(InstagramAvt);
