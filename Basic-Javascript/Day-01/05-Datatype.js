e78d47bd2c23423dddd29f2003cd3c8fd0121040
joyalshaji135 <joyalshaji135@gmail.com>
2024-01-07 19:00:56 +0530

    Datatype are Completed

--- Basic-Javascript/Day-01/05-Datatype.js
+++ Basic-Javascript/Day-01/05-Datatype.js
@@ -1,16 +1,31 @@
 // Datatypes in Javascript
 
 let FatherAge = 34;
-console.log(typeof FatherAge);
+console.log(typeof FatherAge); //Integer
 
 let FatherName = "Shaji Abraham";
-console.log(typeof FatherName);
+console.log(typeof FatherName); //String
 
 let FatherStatus = true;
-console.log(typeof FatherStatus);
+console.log(typeof FatherStatus); //Boolean
 
 let FatherDate;
-console.log(typeof FatherDate);
+console.log(typeof FatherDate); //Undefined
 
 let FatherAccount = null;
-console.log(typeof FatherAccount);
+console.log(typeof FatherAccount); // Null
+
+let Age = BigInt(3922);
+
+console.log(typeof Age); // BigInt
+
+let Sym = Symbol(238);
+
+console.log(typeof Sym); // Symbol
+
+
+// There are Two types of Datatypes
+
+// 1) Primitive Datatype ==> Integer,String,Null,Undefined,Boolean,Bigint,Symbol
+
+// 2) Non-Primitive Datatype ==> Object,Array,Function
