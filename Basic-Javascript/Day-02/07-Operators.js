5b249fefb4764b0ebd08f696e20c224955a7c3bc
joyalshaji135 <joyalshaji135@gmail.com>
2024-01-07 21:20:33 +0530

    Operator in js

---
+++ Basic-Javascript/Day-02/07-Operators.js
@@ -0,0 +1,68 @@
+// Arithematic Operations
+
+let number1 = 60;
+let number2 = 30;
+
+console.log("number1 =",number1,"& number2 =",number2);
+
+// Addition Of Two Numbers
+console.log("number1 + number2 = ",number1+number2);
+
+// Subtraction of  Two Numbers
+console.log("number1 - number2 = ",number1-number2);
+
+// Multiplication of Two Numbers
+console.log("number1 * number2 = ",number1*number2);
+
+// Division of Two Numbers 
+console.log("number1 / number2 =",number1/number2);
+
+// Modules Operations
+
+let mode1 = 10;
+let mode2 = 20;
+
+console.log("Mod1 % Mod2 = ",mode1%mode2);
+
+// Exponentiation Operations
+
+let Number = 2;
+let Power = 3;
+
+console.log("Number ** Power = ",Number ** Power); // 2^3 = 2*2*2 = 8
+
+// Unary Operations
+
+// There are Two Types of Unary Operators 1) Increment and 2) Decrement
+
+// Post Increment And Decrement 
+let inc = 1;
+
+ // inc = inc + 1; post 
+
+console.log("inc post = ",inc++); // out => 1
+console.log("inc after post = ",inc); // out => 2
+
+
+let dec = 1;
+
+// dec = dec - 1; post
+
+console.log("dec post = ",dec--); // out => 1
+console.log("dec after post = ",dec); // out => 0
+
+// Pre Increment and Decrement
+
+let inc1 = 1;
+
+// inc1 = 1 + inc1; pre
+
+console.log("inc pre =  ",++inc1); // out => 2
+console.log("inc after pre = ",inc1); // out => 2
+
+let dec1 = 1;
+
+// dec1 = 1 - dec1;
+
+console.log("dec pre = ",--dec1); // out => 0
+console.log("dec after pre = ",dec1); // out => 0
